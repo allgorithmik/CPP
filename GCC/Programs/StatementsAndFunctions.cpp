0d8138bad48e3057601b2cfc63ce6e57b6ffea1f
allgorithmik <vvishwas2@gmail.com>
2022-01-27 17:39:27 +0530

    Statemnts and Functions

---
+++ GCC/Programs/StatementsAndFunctions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(int argc, char **argv){
+    int first {5};
+    int second {10};
+
+    int result = first + second;
+
+    std::cout << result << std::endl;
+
+    return 0;
+}
--- MSVC/main.cpp
+++ MSVC/main.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 
-int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+int add(int a, int b){
+    int result = a + b;
+    return result;
+}
+
+int main(int argc, char **argv){
+    int first {5};                              // modern way of initialising variables
+    int second {10};                            // modern way of initialising variables
+
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first + second << std::endl;   //another result of sum
+
+    int sum = add(55, 55);                      //calling function
+
+    std::cout << sum << std::endl;
+
+    std::cout << add(45,45) << std::endl;       //calling function within cout -> possible
+
     return 0;
 }
