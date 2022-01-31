b199fa4d8ae7048784cab62d2981e92868668071
allgorithmik <vvishwas2@gmail.com>
2022-01-31 18:35:07 +0530

    Relational Operators

--- MSVC/Programs/CompoundAssignment.cpp
+++ MSVC/Programs/CompoundAssignment.cpp
@@ -3,8 +3,8 @@
 int main(){
     int number1 {100};
 
-    number1 += 1;
-    std::cout << "Compound assignment for addition: " << number1 << std::endl;          //compound operator is not accepted as expression in cout statement???
+    number1 += 1;                                                                       //compound operator is not accepted as expression in cout statement???
+    std::cout << "Compound assignment for addition: " << number1 << std::endl;          //->PUT THE EXPRESSION IN ()         
 
     number1 -= 1;                                                                       //The REAL CONCEPT name is COMPOUND ASSIGNMENT OPERATOR, which means it preforms the
     std::cout << "Compound assignment for subtraction: " << number1 << std::endl;       //operation and then ASSIGNS the new value to the variables.
