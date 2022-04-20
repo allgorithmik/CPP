bbb138fc3fb7c1439218688f996665068e0037d3
allgorithmik <vvishwas2@gmail.com>
2022-04-21 00:29:03 +0530

    The this Pointer => Refers to the Current Object / Class => Can be used on Constructors / Can be used to resolve member variables with same name / Can be used in Chaining Function Calls using Pointers / Can be used in Chaining Function Calls using References

--- GCC/main.cpp
+++ GCC/main.cpp
@@ -1,35 +1,101 @@
 #include<iostream>
 
-//SIZE OF OBJECTS IN C++ WILL BE THE SUMM OF ALL THE DATATYPE SIZES OF ITS MEMBER VARIABLES => BUT THIS IS NOT ALWAYS TRUE BECAUSE OF A CONCEPT CALLED
-//BOUNDARY ALIGNMENT
-//BOUNDARY ALIGNMENT HAPPENS WHEN THERE ARE MEMBER VARIABLES OF DIFFERENT TYPES (int, double, char etc) => DUE TO THIS ALL THE SIMILAR TYPES WILL BE GROUPED TOGETHER =>
-//ints WILL BE STORED IN MEMORY ADDRESSES ON MULTIPLE OF 4 AS ints ARE OF 4 BYTES SIZE AND SO ON FOR ALL TYPES => THIS WILL CAUSE GAPS IN THE MEMORY AND OUR MEMORY SUM
-//WILL BE OFF A LITTLE BIT
+//THE this POINTER:
+//EACH CLASS MEMBER FUNCTION CONTAINS A HIDDEN POINTER CALLED this. IT CONTAINS THE ADDRESS OF THE CURRENT OBJECT FOR WHICH THE METHOD IS BEING EXECUTED. this ALSO APPLIES TO
+//CONSTRUCTORS AND DESTRUCTORS => this POINTER CALLS A SPECIAL FUNCTION WHICH RETURNS THE CURRENT OBJECTS ADDRESS OR DOES THE this IMPLEMENTATION
 
-class Wrapper{
+class Dog{
+    std::string dog_name;
+    std::string dog_breed;
+    int *age {nullptr};
+
     public:
-        void do_something();
-        void print_something();
+        Dog();
+        Dog(std::string name_param, std::string breed_param, int age_param);
 
-    private:                                                                    //note that private comes after public,this is no problem as access spcifiers are mentioned  
-        int var1 {};                                                            //8 bytes
-        int var2 {};                                                            //8 bytes
-        int *p_var {nullptr};                                                   //8 bytes as this is only the size of the pointer and not whats its pointing to
-        std::string str1 {"Mars will be colonzed by Humans"};                   //unspecified number of bytes as std::string is represented as a class internally and its
-};                                                                              //referred by a pointer
+        //FUNCTION PROTOTYPES FOR CHAINED FUNCTION CALLS USING POINTERS
+        Dog *dog_behave();
+        Dog *set_name(const std::string &dog_param);
+        Dog *set_breed(const std::string &dog_breed);
 
-void Wrapper::do_something(){                                                   //functions are NOT counted for size as they are affiliated to the class and not object
+        //FUNCTION PROTOTYPES FOR CHAINED FUNCTION CALLS USING REFERENCES
+        Dog &dog_behave_ref();
+        Dog &set_name_ref(const std::string &dog_param);
+        Dog &set_breed_ref(const std::string &dog_breed);
 
+};
+
+//USING this TO REFER TO CONSTRUCTORS
+Dog::Dog(){
+    std::cout << "This is a normal constructor from " << this << std::endl;         //this POINTS TO THE CURRENT ADDRESS OF NORMAL Dog() CONSTRUCTOR
 }
 
-void Wrapper::print_something(){                                                //functions are NOT counted for size as they are affiliated to the class and not object
-    std::cout << "Want to print something? Here you go!" << std::endl;
+//USING this TO REFER TO CONSTRUCTORS
+Dog::Dog(std::string name_param, std::string breed_param, int age_param){
+    dog_name = name_param;
+    dog_breed = breed_param;
+    age = new int;
+    *age = 10;
+    std::cout << "This is a parameterized constructor from " << this << std::endl;  //this POINTS TO THE CURRENT ADDRESS OF PARAMETERIZED Dog(<params>) CONSTRUCTOR
+}
+
+//FUNCTION DEFINITIONS FOR CHAINED CALL OF FUNCTIONS USING POINTERS
+Dog *Dog::dog_behave(){                                                             //NOTE THAT THIS FUNCTON RETURNS A POINTER
+    std::cout << "This dog behaves" << std::endl;
+    return this;                                                                    //return this IS MANDATORY FOR USING THIS FUNCTION FOR CHAINED CALLS USING POINTERS
+}
+
+Dog *Dog::set_name(const std::string &dog_name){                                    //NOTE THAT THIS FUNCTON RETURNS A POINTER
+    this -> dog_name = dog_name;                                                    //HERE A MEMBER VARIABLE WITH SAME NAME IS RESOLVED BY THE this -> POINTER
+    //dog_name = dog_name;                                                            //COMPILER ERROR => COMPILER DOES NOT KNOW WHICH dog_name WE ARE REFERRING TO
+    return this;                                                                    //return this IS MANDATORY FOR USING THIS FUNCTION FOR CHAINED CALLS USING POINTERS
+}
+
+Dog *Dog::set_breed(const std::string &dog_breed){                                  //NOTE THAT THIS FUNCTON RETURNS A POINTER
+    this -> dog_breed = dog_breed;                                                  //HERE A MEMBER VARIABLE WITH SAME NAME IS RESOLVED BY THE this -> POINTER
+    return this;                                                                    //return this IS MANDATORY FOR USING THIS FUNCTION FOR CHAINED CALLS USING POINTERS
+}
+
+//FUNCTION DEFINITIONS FOR CHAINED CALL OF FUNCTIONS USING REFERENCES
+Dog &Dog::dog_behave_ref(){                                                         //NOTE THAT THIS FUNCTON RETURNS A REFERENCE
+    std::cout << "This dog behaves" << std::endl;
+    return *this;                                                                   //DEREFERENCING *this IS MANDATORY FOR USING FUNCTION IN CHAINED CALLS USING REFERENCES
+}
+
+Dog &Dog::set_name_ref(const std::string &dog_name){                                //NOTE THAT THIS FUNCTON RETURNS A REFERENCE
+    this -> dog_name = dog_name;                                                    //HERE A MEMBER VARIABLE WITH SAME NAME IS RESOLVED BY THE this -> POINTER
+    //dog_name = dog_name;                                                            //COMPILER ERROR => COMPILER DOES NOT KNOW WHICH dog_name WE ARE REFERRING TO
+    return *this;                                                                   //DEREFERENCING *this IS MANDATORY FOR USING FUNCTION IN CHAINED CALLS USING REFERENCES
+}
+
+Dog &Dog::set_breed_ref(const std::string &dog_breed){                              //NOTE THAT THIS FUNCTON RETURNS A REFERENCE
+    this -> dog_breed = dog_breed;                                                  //HERE A MEMBER VARIABLE WITH SAME NAME IS RESOLVED BY THE this -> POINTER
+    return *this;                                                                   //DEREFERENCING *this IS MANDATORY FOR USING FUNCTION IN CHAINED CALLS USING REFERENCES
 }
 
 int main(int argc, char **argv){
 
-    Wrapper wrap;
-    std::cout << "Size of objects in C++ is: " << sizeof(wrap) << std::endl;    //size of objects in C++ will be the sum of all the datatype sizes of its member variables
+    Dog dog1("Canine1", "Dog", 10);
+    Dog *p_dog = new Dog("Canine2", "Wolf", 10);
+
+    std::cout << "USING POINTER NOTATION TO PERFORM CHAINED CALLS USING POINTERS" << std::endl;
+    p_dog -> dog_behave();                                                          //USING POINTER NOTATION TO ACCESS MEMBERS OF A CLASS
+
+    //CHAINED CALLS USNG POINTERS:
+    //*****PLEASE NOTE***** => FOR A FUNCTION TO BE USED IN CHAINING USING POINTERS:
+    //IT MUST RETURN A POINTER
+    //IT MUST BE OF TYPE POINTER CLASS / OBJECT
+    //IT MUST RETURN THE PONTER => return this
+    p_dog->dog_behave()->set_name("Hello")->set_breed("Canine");                    //CHAINED CALL OF FUNCTIONS USING POINTERS => -> NOTATION
+    delete p_dog;
+
+    //CHAINED CALLS USING REFERENCES:
+    //*****PLEASE NOTE***** => FOR A FUNCTION TO BE USED IN CHAINING USING REFERENCES:
+    //IT MUST RETURN A POINTER TO this
+    //IT MUST BE OF REFERENCE TYPE OF ITS CLASS / OBJECT
+    //IT MUST RETURN THE PONTER => return *this
+    Dog *p_dog1 = new Dog("Canine2", "Wolf", 10);
+    p_dog1->dog_behave_ref().set_name_ref("Hello").set_breed_ref("Canine");         //CHAINED CALL OF FUNCTIONS USING REFERENCES => . NOTATION
 
     return 0;
 }
